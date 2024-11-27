cef1bf6e9e79a55382bcea524d7a57eb41a3c57f
20244311 <binjeong421@gmail.com>
2024-11-27 18:15:25 +0900

    Add files via upload

---
+++ 학점계산기.py
@@ -0,0 +1,12 @@
+i = int(input("성점을 입력하세요 :"))
+if i >= 90 :
+    print("학점 A")
+elif i >= 80 :
+    print("학점 B")
+elif i >= 70 :
+    print("학점 C")
+elif i >= 60 :
+    print("학점 D")
+else :
+    print("학점 F")
+
